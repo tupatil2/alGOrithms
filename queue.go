fd2d9623135d4fd22bc8d1bce3a92935c510dcef
= <patiltusharvikas@gmail.com>
2023-05-21 13:52:22 +0530

    implement queue

---
+++ queue.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  queue := Queue{}
+  queue.Enqueue(1)
+  queue.Enqueue(2)
+  queue.Enqueue(3)
+  queue.Enqueue(4)
+  queue.Enqueue(5)
+  queue.Enqueue(6)
+  queue.Enqueue(7)
+
+  fmt.Println("size: ", queue.Size())
+
+  firstElement := queue.Dequeue()
+  fmt.Println("firstElement is ", firstElement)
+
+  fmt.Println("size: ", queue.Size())
+
+  for queue.Size() != 0 {
+    fmt.Println("element is ", queue.Dequeue())
+  }
+
+}
+
+type Queue struct {
+  elements []interface{}
+}
+
+func (q *Queue) Enqueue(item interface{}) {
+  q.elements = append(q.elements, item)
+}
+
+func (q * Queue) Dequeue() interface{} {
+  if len(q.elements) == 0 {
+    return nil
+  }
+  item := q.elements[0]
+  q.elements = q.elements[1:]
+  return item
+}
+
+func (q *Queue) Size() int {
+  return len(q.elements)
+}
